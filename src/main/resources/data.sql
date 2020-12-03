d30564c2940d39d675931aef6329978bd9994a62
Patrick Schmidt <schmidt@ugd.ai>
2020-12-03 18:17:32 +0100

    added default data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+insert into user (id, email, password, name, surname)
+values
+       ( 1, 'user@shop.de', 'password', 'Peter', 'Parker' ),
+       ( 2, 'admin@shop.de', 'password', 'Hulk', 'Hogan' );
+
+insert into item (id, title, comment, costs)
+values
+       ( 1000, 'Beer', 'Cheers!', 0.0 ),
+       ( 1001, 'RTX 3080', 'currently unavailable', 999.00 ),
+       ( 1002, 'Space X Flight', 'One Way to Mars', 150000.99 );
+
